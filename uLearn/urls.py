86644e73c7e11835bf0c4dff0061bd83c3845a26
yilmazalican <yilmazalican@outlook.com>
2017-02-28 13:49:21 +0200

    minor

--- uLearn/urls.py
+++ uLearn/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from uLearnApp import views
 
 urlpatterns = [
-    url(r'^', include('uLearnApp.urls')),
+    url(r'^/', include('uLearnApp.urls')),
     url(r'^admin/', admin.site.urls),
+
 ]
