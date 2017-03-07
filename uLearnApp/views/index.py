5611f24addbbce2ec7f4bf4aadfd2217646b5b10
MAC ADMIN <macadmin@kusadas.local>
2017-03-07 13:50:43 +0200

    index page

--- uLearnApp/views/index.py
+++ uLearnApp/views/index.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def anaSayfa(request):
-    alican = 5
-    return render(request,'index.html', {'ertan':alican})
+    return render(request,'index.html')
