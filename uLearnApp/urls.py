c138ca13c22e3b417d156458037977b84d184d31
MAC ADMIN <macadmin@kusadas.local>
2017-03-07 12:04:53 +0200

    loginpage

--- uLearnApp/urls.py
+++ uLearnApp/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, include
 
-from .views import index,lecture
+from .views import index,lecture,login
 
 urlpatterns = [
     url(r'^$', index.anaSayfa, name='index'),
     url(r'lectures', lecture.lecture, name='index'),
-
+    url(r'login', login.login, name='login'),
 ]
