0883b579131b95f07280aa3ca86d46c826af7a3e
yilmazalican <yilmazalican@outlook.com>
2017-03-09 09:55:35 +0200

    register cont++

---
+++ uLearnApp/forms.py
@@ -0,0 +1,2 @@
+class RegisterForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
--- uLearnApp/views/register.py
+++ uLearnApp/views/register.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.db import models
 
 def register(request):
-
+    email = request.POST.get('email')
+    name = request.POST.get('name')
+    surname = request.POST.get('surname')
+    password = request.POST.get('password')
+    bdate = request.POST.get('bdate')
+    user = User(username = email,email = email,first_name = name, last_name = surname, password = password)
+    myuser = MyUser(user = user, birth_date = bdate)
+    user.save()
+    myuser.save()
     return render(request,'register.html')
