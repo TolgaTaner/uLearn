17fed9057dd78c1a644c1bb9899d2791fce09f2e
yilmazalican <yilmazalican@outlook.com>
2017-03-23 09:57:33 +0200

    modelforms ++

--- uLearnApp/forms.py
+++ uLearnApp/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.models import User
+from .models import MyUser
 
 class RegisterForm(forms.ModelForm):
         email = forms.CharField(widget = forms.EmailInput(attrs={'class': 'required form-control', 'placeholder': 'Email*'}),label = '')
@@ -6,4 +8,8 @@ class RegisterForm(forms.ModelForm):
         surname = forms.CharField(widget = forms.TextInput(attrs={'class': 'required form-control', 'placeholder': 'Surname*'}),label = '')
         password = forms.CharField(widget = forms.PasswordInput(attrs={'class': 'required form-control', 'placeholder': 'Password*'}),label = '')
         conform_password = forms.CharField(widget = forms.PasswordInput(attrs={'class': 'required form-control', 'placeholder': 'Conform password*'}),label = '')
-        bdate = forms.CharField(widget = forms.TextInput(attrs={'type':'date','class':'required form-control','id':'exampleInputDOB1'}),label = '')
+        birth_date = forms.CharField(widget = forms.TextInput(attrs={'type':'date','class':'required form-control','id':'exampleInputDOB1'}),label = '')
+        user = User()
+        class Meta:
+            model = MyUser
+            fields = ['birth_date']
--- uLearnApp/views/register.py
+++ uLearnApp/views/register.py
@@ -3,13 +3,24 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.db import models
 from uLearnApp.forms import RegisterForm
+from uLearnApp.models import MyUser
+from django.forms import inlineformset_factory
 
 def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
+        user_formset = inlineformset_factory(User, MyUser , fields=('birth_date',))
+        email = form.cleaned_data('email')
+        password = form.cleaned_data('password')
+        name = form.cleaned_data('name')
+        surname = form.cleaned_data('surname')
+        user = User.objects.create_user(email, email = email, password = password, first_name = name, last_name = surname)
+        formset = user_formset(user = user)
+        if formset.is_valid():
+            formset.save()
             return HttpResponseRedirect('/')
+
+
     else:
         form = RegisterForm()
     return render(request, 'register.html', {'form': form})
